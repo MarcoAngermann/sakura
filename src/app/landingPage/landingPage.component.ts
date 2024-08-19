6591d5ab81a2a5971eab090b6e555e3b99778a3b
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-08-19 13:18:02 +0200

    sakura angular

---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+@Component({
+    selector: 'app-landingPage',
+    standalone: true,
+    imports: [CommonModule, NavbarComponent],
+    template: `
+    <section>
+        <div><app-navbar></app-navbar></div> 
+        <h1 class="font-raleway">SAKURA RAMEN</h1>
+        <h2 class="font-raleway">BEST RAMEN IN TOWN</h2>
+    </section>
+        `,
+    styleUrls: ['./landingPage.component.scss'],
+})
+export class LandingPageComponent {}
