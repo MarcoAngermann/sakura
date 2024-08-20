3803b418a0b2e6e158dc905b5c191d9978d2cdac
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-08-20 13:26:45 +0200

    main-content

--- src/app/main-content/landing-page/landingPage.component.ts
+++ src/app/main-content/landing-page/landingPage.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 @Component({
     selector: 'app-landingPage',
     standalone: true,
-    imports: [CommonModule, NavbarComponent],
+    imports: [NavbarComponent],
     template: `
     <section>
         <div><app-navbar></app-navbar></div> 
