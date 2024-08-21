a5921ca24eed02d688d063188d330c6cc19b2250
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-08-21 12:55:03 +0200

    our menu

--- src/app/main-content/landing-page/landingPage.component.ts
+++ src/app/main-content/landing-page/landingPage.component.ts
@@ -8,7 +8,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     <section>
         <div><app-navbar></app-navbar></div> 
         <h1 class="font-raleway">SAKURA RAMEN</h1>
-        <h2 class="font-raleway">BEST RAMEN IN TOWN</h2>
+        <h2 class="font-raleway">THE BEST RAMEN IN TOWN</h2>
     </section>
         `,
     styleUrls: ['./landingPage.component.scss'],
